12898b8f8629c4a6e42e755f1232a9452e1f2f96
moitoif <yadaobo35456@126.com>
2021-04-21 15:44:32 +0800

    ユーザー登録画面レイアウト作成。その他ファイル更新。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # トップ
+  get "/" => "home#top"
+
+  # ユーザ
+  # 新規登録
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+
+
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  # 画像アップロード
+  get "images" => "images#index"
+
 end
