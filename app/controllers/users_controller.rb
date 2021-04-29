f1ff1f92e34f40a5d406b69b02afae123aee1a11
moitoif <yadaobo35456@126.com>
2021-04-29 23:32:22 +0800

    アカウント登録処理実装 合わせてアカウント登録画面モック、bcryptのバージョンアップ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password = params[:password]
+
+    if @user.save
+      flash[:notice] = "アカウント作成が完了しました。"
+      redirect_to("/users/#{@user.id}")
+    end
   end
 end
