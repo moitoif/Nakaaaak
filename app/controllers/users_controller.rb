63a063e4155ad1c84423e7c505e0ac815ed1fb11
moitoif <yadaobo35456@126.com>
2021-05-05 07:35:52 +0800

    【モック】ログイン処理実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,12 @@ class UsersController < ApplicationController
   end
 
   def login
-    
+    user = User.find_by(email: params[:email], 
+                        password_digest: params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to("/users/#{user.id}")
+    end
   end
 
   def new
