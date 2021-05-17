6320fe6980ec78a025c5edda768389f11ccb8513
moitoif <yadaobo35456@126.com>
2021-05-17 15:24:22 +0800

    ログイン後の遷移先をマイページに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
   end
+
+  def after_sign_in_path_for(resource)
+    # pages_show_path
+    mypage_path
+  end
 end
