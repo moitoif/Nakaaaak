83870170a8871e58772bddaeba07d0df48a43fd2
moitoif <yadaobo35456@126.com>
2021-05-22 23:10:52 +0800

    Userコントローラのdevise対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  validates :name, {presence: true}
-  validates :email, {presence: true, uniqueness: true}
 
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   
 end
