732aa444c325574ba40b3da3cab04dd5661616b3
moitoif <yadaobo35456@126.com>
2021-06-01 18:59:53 +0800

    画像アップロード作成

--- app/controllers/illusts_controller.rb
+++ app/controllers/illusts_controller.rb
@@ -1,10 +1,34 @@
 class IllustsController < ApplicationController
+  before_action :authenticate_user!
 
   def upload_form
-    
+    @current_user = current_user.id 
   end
 
   def upload
+    image = Image.new
+    image.user_id = current_user.id
+    image.name = params[:title]
+    image.content = params[:caption]
 
+    if params[:illust]
+      # ファイル名は、「今日の日付+連番４桁」
+      image.filename = image.get_filename(current_user.id)
+      directory_path = "public/users/#{current_user.id}"
+
+      # ディレクトリがあるかどうかチェック
+      unless File.directory?(directory_path)
+        # なかったらディレクトリを作成する。
+        Dir.mkdir(directory_path)
+      end
+      File.binwrite("public/users/#{current_user.id}/#{image.filename}", params[:illust].read)
+    end
+
+    if image.save
+      flash[:notice] = "投稿しました"
+      redirect_to("/mypage")
+    else
+      render("illusts/upload_form")
+    end
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,16 @@
 class Image < ApplicationRecord
+
+  def get_filename(current_user_id)
+    image = Image.where(user_id: current_user_id).maximum(:filename)
+
+    if image.nil?
+      return "#{Date.today.strftime("%Y%m%d")}0001.jpg"
+    else
+      image = image.delete(".jpg")
+    end
+
+    serial = image.to_i + 1
+
+    return "#{serial}.jpg"
+  end
 end
