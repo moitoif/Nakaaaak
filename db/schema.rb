6f8ae8a9e9d5573b78b289defd0b210c07790d0c
moitoif <yadaobo35456@126.com>
2021-08-02 03:30:43 +0800

    質問用テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_065624) do
+ActiveRecord::Schema.define(version: 2021_04_28_083611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2021_03_21_065624) do
     t.datetime "updated_at", null: false
     t.string "filename"
     t.boolean "answer_flg"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.bigint "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
