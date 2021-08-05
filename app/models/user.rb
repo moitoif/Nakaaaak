48e8e2402fb374ff4e904a2fd723f9d5ed6d4197
moitoif <yadaobo35456@126.com>
2021-08-05 12:46:53 +0800

    リレーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :comments, foreign_key: 'comment_user_id'
   
 end
