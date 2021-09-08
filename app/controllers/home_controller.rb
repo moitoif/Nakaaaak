f23503c08c4ed88425dd640e621503a080cc1cd8
moitoif <yadaobo35456@126.com>
2021-09-09 00:34:23 +0800

    トップ画面レイアウト作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def top
-    @illusts = Image.where(comment_flg: false).order(updated_at: :desc).limit(5)
+    @illusts = Image.where(comment_flg: false).order(updated_at: :desc).limit(10)
   end
 end
