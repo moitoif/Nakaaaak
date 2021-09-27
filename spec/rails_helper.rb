b390cea0d9c4a99a338855b76a60ca6400ff47e4
moitoif <yadaobo35456@126.com>
2021-09-28 03:48:14 +0800

    メソッド名変更、不要なヘルパー削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,6 +63,5 @@ RSpec.configure do |config|
 
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
 end
