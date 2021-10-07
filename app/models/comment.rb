3a60d3115bc489803e337dde93efbd2aa8b79306
moitoif <yadaobo35456@126.com>
2021-10-07 20:12:24 +0800

    コメントモデルテスト

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
+
+  validates :comment,
+    length: { maximum: 500 }
+
   belongs_to :user, optional: true
   has_many :image, foreign_key: 'id'
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "コメントが500文字入力されているとき有効であるか" do
+    comment = Comment.new
+    comment.comment = "a" * 500
+    comment.valid?
+    expect(comment).to be_valid
+  end
+
+  it "コメントが500文字を超過しているとき" do
+    comment = Comment.new
+    comment.comment = "a" * 501
+    comment.valid?
+    expect(comment.errors.full_messages_for(:comment)).to include("コメントは500文字以内で入力してください")
+  end
 end
